4bfc0c5424f575d6d6f03afd28a3dfe62f203bdd
Flux Xu <fluxxu@gmail.com>
2016-05-18 03:16:09 -0400

    0.3.0

--- hmr.js
+++ hmr.js
@@ -1,230 +1,284 @@
+//////////////////// HMR BEGIN ////////////////////
 if (module.hot) {
-  try {
-    (function(originalElm) {
-      "use strict";
-      var instances = module.hot && module.hot.data
-        ? module.hot.data.instances || {}
-        : {};
-      var uid = module.hot && module.hot.data
-        ? module.hot.uid || 0
-        : 0;
+  (function(Elm) {
+    "use strict";
 
-      var Elm = wrapElm(originalElm);
+    var programWithFlags = _elm_lang$virtual_dom$VirtualDom$programWithFlags;
 
-      module.hot.accept();
-      module.hot.dispose(function(data) {
-        data.instances = instances;
-        data.uid = uid;
-      });
+    if (!programWithFlags) {
+      console.warn('[elm-hot] Hot-swapping disabled because VirtualDom module was not found.')
+      return;
+    }
 
-      Object.keys(instances).forEach(function(id) {
-        var instance = instances[id];
-        var path = instance.name;
-        console.log('[elm-hot] Swapping module: ' + path + '#' + id);
-        var oldElm = instance.elm;
-        var hookedDispose = oldElm.dispose;
-        var newModule = getModule(Elm, path);
-        var i;
-        if (!newModule) {
-          console.error('[elm-hot] module to swap not found, path: ' + path);
-          return
+    var instances = module.hot.data
+      ? module.hot.data.instances || {}
+      : {};
+    var uid = module.hot.data
+      ? module.hot.uid || 0
+      : 0;
+
+    var cancellers = [];
+
+    var initializingInstance = null, swappingInstance = null;
+
+    module.hot.accept();
+    module.hot.dispose(function(data) {
+      data.instances = instances;
+      data.uid = uid;
+      if (cancellers.length) {
+        console.log('[elm-hot] Killing ' + cancellers.length + ' running processes...');
+        try {
+          cancellers.forEach(function (cancel) {
+            cancel();
+          });
+        } catch (e) {
+          console.warn('[elm-hot] Kill process error: ' + e.message);
         }
+      }
+    });
 
-        //copy id and class: https://github.com/fluxxu/elm-hot-loader/issues/5
-        var container = instance.container;
-        var containerParent, containerIndex, containerClass, containerId;
-        if (container) {
-          containerParent = instance.container.parentNode;
-          containerIndex = -1;
-          containerClass = container.className;
-          containerId = container.id;
-          for (i = 0; i < container.parentNode.childNodes.length; i++) {
-            if (container.parentNode.childNodes[i] === container) {
-              containerIndex = i;
-            }
-          }
-          if (containerIndex === -1) {
-            console.error('[elm-hot] Can not find container.');
-            return
-          }
-        }
+    function getId() {
+      return ++uid;
+    }
 
-        var newElm = instance.elm = oldElm.swap(newModule);
-
-        if (container) {
-          instance.container = containerParent.childNodes[containerIndex];
-          instance.container.className = containerClass;
-          instance.container.id = containerId;
-        }
-
-        hookedDispose.original = newElm.dispose;
-        newElm.dispose = hookedDispose;
-
-        //reconnect ports
-        var portSubscribes = instance.portSubscribes;
-        var portNames;
-        if (portSubscribes) {
-          portNames = Object.keys(portSubscribes);
-          if (portNames.length) {
-            portNames.forEach(function(name) {
-              var handlers = portSubscribes[name];
-              if (!handlers.length) {
-                return;
-              }
-              console.log('[elm-hot] Reconnect ' + handlers.length + ' handler(s) to port \'' + name + '\' (' + path + ').');
-              handlers.forEach(function(handler) {
-                newElm.ports[name].subscribe(handler);
-              });
-
-              wrapPorts(newElm, portSubscribes);
-            });
-          }
-        }
-
-        if ('swap' in newElm.ports) {
-          //trigger re-render
-          newElm.ports.swap.send(true)
+    function findPublicModules(parent, path) {
+      var modules = [];
+      for (var key in parent) {
+        var child = parent[key];
+        var currentPath = path ? path + '.' + key : key;
+        if ('fullscreen' in child) {
+          modules.push({
+            path: currentPath,
+            module: child
+          });
         } else {
-          console.error('[elm-hot] \'swap\' port is not defined.');
+          modules = modules.concat(findPublicModules(child, currentPath));
         }
-      });
-
-      module.exports = Elm;
-
-      function getModule(elm, path) {
-        var parts = path.split('.');
-        var parent = elm;
-        var part, prop;
-        for (var i = 0; i < parts.length; i++) {
-          part = parts[i];
-          prop = parent[part];
-          if (prop && typeof prop === 'object') {
-            parent = prop;
-          } else {
-            parent = null;
-            break;
-          }
-        }
-        return parent;
       }
+      return modules;
+    }
 
-      function findModulePath(elm, module) {
-         var path = find('', elm, module);
-         if (!path) {
-           throw new Error('[elm-hot] Can not resolve module path inside elm instance.')
-         }
-         console.log('[elm-hot] module found at Elm.' + path)
-         return path;
+    function getPublicModule(path) {
+      var parts = path.split('.');
+      var parent = Elm;
+      for (var i = 0; i < parts.length; ++i) {
+        var part = parts[i];
+        if (part in parent) {
+          parent = parent[part]
+        }
+        if (!parent) {
+          return null;
+        }
+      }
+      return parent
+    }
 
-         function find(path, parent, module) {
-          var prop, propPath, nested;
-          for (var key in parent) {
-            prop = parent[key];
-            propPath = path ? (path + '.' + key) : key;
-            if (prop === module) {
-              return propPath
-            } else {
-              if (prop && typeof prop === 'object') {
-                nested = find(propPath, prop, module)
-                if (nested) {
-                  return nested
-                }
-              }
+    function registerInstance(domNode, flags, path, portSubscribes) {
+      var id = getId();
+      return instances[id] = {
+        id: id,
+        path: path,
+        domNode: domNode,
+        flags: flags,
+        portSubscribes: portSubscribes,
+        elmProxy: null
+      };
+    }
+
+    function wrapPublicModule(path, module) {
+      var embed = module.embed;
+      var fullscreen = module.fullscreen;
+      module.embed = function(domNode, flags) {
+        var elm;
+        var portSubscribes = {};
+        initializingInstance = registerInstance(domNode, flags, path, portSubscribes)
+        elm = embed(domNode, flags);
+        wrapPorts(elm, portSubscribes)
+        elm = initializingInstance.elmProxy = {
+          ports: elm.ports
+        };
+        initializingInstance = null;
+        return elm;
+      };
+
+      module.fullscreen = function (flags) {
+        var elm
+        var portSubscribes = {};
+        initializingInstance = registerInstance(document.body, flags, path, portSubscribes)
+        elm = fullscreen(flags);
+        wrapPorts(elm, portSubscribes)
+        elm = initializingInstance.elmProxy = {
+          ports: elm.ports
+        };
+        initializingInstance = null;
+        return elm;
+      }
+    }
+
+    function swap(instance) {
+      console.log('[elm-hot] Hot-swapping module: ' + instance.path)
+
+      swappingInstance = instance;
+
+      var domNode = instance.domNode;
+
+      while (domNode.lastChild) {
+        domNode.removeChild(domNode.lastChild);
+      }
+      
+      var m = getPublicModule(instance.path)
+      var elm;
+      if (m) {
+        if (instance.isFullscreen) {
+          elm = m.fullscreen(instance.flags);
+        } else {
+          elm = m.embed(domNode, instance.flags);
+        }
+
+        instance.elmProxy.ports = elm.ports;
+
+        Object.keys(instance.portSubscribes).forEach(function(portName) {
+          if (portName in elm.ports && 'subscribe' in elm.ports[portName]) {
+            var handlers = instance.portSubscribes[portName];
+            if (!handlers.length) {
+              return;
             }
-          }
-         }
-      }
-
-      function wrapElm(Elm) {
-        return Object.assign({}, Elm, {
-          embed: function(module, container, config) {
-            return wrap(module, container, config);
-          },
-          fullscreen: function(module, config) {
-            return wrap(module, null , config);
+            console.log('[elm-hot] Reconnect ' + handlers.length + ' handler(s) to port \'' + portName + '\' (' + instance.path + ').');
+            handlers.forEach(function(handler) {
+              elm.ports[portName].subscribe(handler);
+            });
+          } else {
+            delete instance.portSubscribes[portName];
+            console.log('[elm-hot] Port was removed: ' + portName);
           }
         });
-
-        function getUID() {
-          return ++uid;
-        }
-
-        function wrap(module, container, config) {
-          var id = getUID();
-          var embed = originalElm.embed;
-          var fullscreen = originalElm.fullscreen;
-
-          //find module name
-          var name = findModulePath(Elm, module)
-
-          var elm = container
-            ? embed(module, container, config)
-            : fullscreen(module, config);
-
-          //hook dispose
-          var dispose = elm.dispose;
-          var hookedDispose = function() {
-            delete instances[id];
-            return dispose();
-          };
-
-          hookedDispose.original = dispose;
-          elm.dispose = hookedDispose;
-
-          //register
-          instances[id] = {
-            elm: elm,
-            name: name,
-            container: container,
-            portSubscribes: wrapPorts(elm, {})
-          };
-
-          return elm;
-        }
+      } else {
+        console.log('[elm-hot] Module was removed: ' + instance.path);
       }
 
-      //hook ports to reconnect after swap
-      function wrapPorts(elm, portSubscribes) {
-        var portNames = Object.keys(elm.ports);
-        //hook ports
-        if (portNames.length) {
-          portNames
-            .filter(function(name) {
-              return 'subscribe' in elm.ports[name];
-            })
-            .forEach(function(portName) {
-              var port = elm.ports[portName];
-              var subscribe = port.subscribe;
-              var unsubscribe = port.unsubscribe;
-              elm.ports[portName] = Object.assign(port, {
-                subscribe: function(handler) {
-                  console.log('[elm-hot] ports.' + portName + '.subscribe called.');
-                  if (!portSubscribes[portName]) {
-                    portSubscribes[portName] = [ handler ];
-                  } else {
-                    //TODO handle subscribing handler more than once?
-                    portSubscribes[portName].push(handler);
-                  }
-                  return subscribe.call(port, handler);
-                },
-                unsubscribe: function(handler) {
-                  console.log('[elm-hot] ports.' + portName + '.unsubscribe called.');
-                  var list = portSubscribes[portName];
-                  if (list && list.indexOf(handler) !== -1) {
-                    list.splice(list.lastIndexOf(handler), 1);
-                  } else {
-                    console.warn('[elm-hot] ports.' + portName + '.unsubscribe: handler not subscribed');
-                  }
-                  return unsubscribe.call(port, handler);
+      swappingInstance = null;
+    }
+
+    function wrapPorts(elm, portSubscribes) {
+      var portNames = Object.keys(elm.ports);
+      //hook ports
+      if (portNames.length) {
+        portNames
+          .filter(function(name) {
+            return 'subscribe' in elm.ports[name];
+          })
+          .forEach(function(portName) {
+            var port = elm.ports[portName];
+            var subscribe = port.subscribe;
+            var unsubscribe = port.unsubscribe;
+            elm.ports[portName] = Object.assign(port, {
+              subscribe: function(handler) {
+                console.log('[elm-hot] ports.' + portName + '.subscribe called.');
+                if (!portSubscribes[portName]) {
+                  portSubscribes[portName] = [ handler ];
+                } else {
+                  //TODO handle subscribing to single handler more than once?
+                  portSubscribes[portName].push(handler);
                 }
-              });
+                return subscribe.call(port, handler);
+              },
+              unsubscribe: function(handler) {
+                console.log('[elm-hot] ports.' + portName + '.unsubscribe called.');
+                var list = portSubscribes[portName];
+                if (list && list.indexOf(handler) !== -1) {
+                  list.splice(list.lastIndexOf(handler), 1);
+                } else {
+                  console.warn('[elm-hot] ports.' + portName + '.unsubscribe: handler not subscribed');
+                }
+                return unsubscribe.call(port, handler);
+              }
             });
-        }
-        return portSubscribes;
+          });
       }
-    })(module.exports);
-  } catch (e) {
-    console.error('[elm-hot] crashed. Please report this to https://github.com/fluxxu/elm-hot-loader/issues');
-    console.error(e);
-  }
+      return portSubscribes;
+    }
+
+    // hook program creation
+    _elm_lang$virtual_dom$VirtualDom$programWithFlags = function () {
+      var instance = initializingInstance;
+      var swapping = swappingInstance;
+      var tryFirstRender = !!swappingInstance;
+      var isInitialRender = true;
+      var program = programWithFlags.apply(this, arguments);
+
+      //var makeRenderer = program.renderer;
+      var init = program.init;
+      var view = program.view;
+      program.init = function () {
+        var result = init.apply(this, arguments);
+        if (swapping) {
+          result._0 = swapping.lastState;
+        }
+        return result;
+      };
+      program.view = function(model) {
+        var result;
+        // first render may fail if shape of model changed too much
+        if (tryFirstRender) {
+          tryFirstRender = false;
+          try {
+            result = view(model);
+          } catch (e) {
+            throw new Error('[elm-hot] Hot-swapping is not possible, please reload page.');
+          }
+        } else {
+          result = view(model);
+        }
+        if (instance) {
+          instance.lastState = model;
+        } else {
+          instance = swapping;
+        }
+        isInitialRender = false;
+        return result;
+      };
+      return program;
+    }
+
+    // hook process creation
+    var nativeBinding = _elm_lang$core$Native_Scheduler.nativeBinding
+    _elm_lang$core$Native_Scheduler.nativeBinding = function() {
+      var def = nativeBinding.apply(this, arguments);
+      var callback = def.callback
+      def.callback = function() {
+        var result = callback.apply(this, arguments)
+        if (result) {
+          cancellers.push(result);
+          return function() {
+            cancellers.splice(cancellers.indexOf(result), 1);
+            return result();
+          };
+        }
+        return result;
+      };
+      return def;
+    };
+
+    // swap instances
+    var removedInstances = [];
+    for (var id in instances) {
+      var instance = instances[id]
+      if (instance.domNode.parentNode) {
+        swap(instance);
+      } else {
+        removedInstances.push(id);
+      }
+    }
+
+    removedInstances.forEach(function (id) {
+      delete instance[id];
+    });
+
+    // wrap all public modules
+    var publicModules = findPublicModules(Elm);
+    publicModules.forEach(function (m) {
+      wrapPublicModule(m.path, m.module);
+    });
+  })(Elm);
 }
+//////////////////// HMR END ////////////////////
--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-var readFileSync = require('fs').readFileSync;
-var path = require('path');
-
-var hmrScript = readFileSync(__dirname + '/hmr.js');
-
 /*
   MIT License http://www.opensource.org/licenses/mit-license.php
   Author Flux Xu @fluxxu
 */
+
+var readFileSync = require('fs').readFileSync;
+var path = require('path');
+
+var hmrScript = readFileSync(__dirname + '/hmr.js');
+var injectBeforeCode = 'if (typeof define === "function" && define[\'amd\'])';
+
 var loaderUtils = require("loader-utils");
 module.exports = function(content) {
   this.cacheable && this.cacheable();
   var callback = this.async();
 
   if (!callback) {
-    throw 'elm-hot-loader currently only supports async mode.'
+    throw new Error('elm-hot-loader currently only supports async mode.')
   }
 
   var input = loaderUtils.getRemainingRequest(this);
   process.nextTick(function() {
-    callback(null, wrap(input, content));
+    wrap(input, content, callback);
   });
 };
 
-function wrap(input, content) {
-  return [
-    content,
-    hmrScript
-  ].join('\n');
+function wrap(input, content, callback) {
+  var injectPos = content.indexOf(injectBeforeCode);
+  if (injectPos === -1) {
+    return callback(new Error('elm-hot-loader is incompatible with this version of Elm.'));
+  } else {
+    return callback(null, content.slice(0, injectPos) + '\r\n\r\n' +
+      hmrScript + '\r\n\r\n' +
+      content.slice(injectPos));
+  }
 }
